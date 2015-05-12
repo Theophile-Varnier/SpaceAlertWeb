981a47add72fb5ed1718c15386c7cb9dd34b51fe
Kukukun <theophile.varnier@gmail.com>
2015-05-12 22:36:25 +0200

    config premiere mission

--- SpaceAlert.Model/Jeu/EvenementMenace.cs
+++ SpaceAlert.Model/Jeu/EvenementMenace.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SpaceAlert.Model.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,17 @@ using System.Threading.Tasks;
 
 namespace SpaceAlert.Model.Jeu
 {
+    /// <summary>
+    /// Décrit un événement correspondant à l'arrivée d'une menace
+    /// </summary>
     public class EvenementMenace: Evenement
     {
+        public bool Confirme { get; set; }
+
+        public Zone Zone { get; set; }
+
+        public int TourArrive { get; set; }
+
         public override void Resolve()
         {
             throw new NotImplementedException();
