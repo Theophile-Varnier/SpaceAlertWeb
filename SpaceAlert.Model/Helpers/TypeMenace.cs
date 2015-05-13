4a7a3627f5f730c0828f05e22c1cd17696355fc8
Kukukun <theophile.varnier@gmail.com>
2015-05-13 10:48:49 +0200

    types menace

---
+++ SpaceAlert.Model/Helpers/TypeMenace.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceAlert.Model.Helpers
+{
+    public enum TypeMenace
+    {
+        MENACE_EXTERNE = 0,
+        MENACE_EXTERNE_SERIEUSE,
+        MENACE_INTERNE,
+        MENACE_INTERNE_SERIEUSE
+    }
+}
