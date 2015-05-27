85ad3b1029fd9198eb2b7ccbb66fec7b7f81740c
Kukukun <theophile.varnier@gmail.com>
2015-05-27 14:12:12 +0200

    début jeu

---
+++ SpaceAlert.Web/Scripts/App/jeu.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $("#draggable").draggable({
+        containment: "parent"
+    });
+
+    $("#draggable").on("mouseenter", function() {
+        $(this).attr("src", "../Content/Medias/Guss/selectedblueguss.png");
+    });
+
+    $("#draggable").on("mouseleave", function () {
+        $(this).attr("src", "../Content/Medias/Guss/blueguss.png");
+    });
+})
