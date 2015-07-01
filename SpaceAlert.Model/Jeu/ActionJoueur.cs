45c5a3396103d968fbfe4ab3abb52e42e5e6ea5a
Kukukun <theophile.varnier@gmail.com>
2015-07-01 11:23:38 +0200

    ajout actions

---
+++ SpaceAlert.Model/Jeu/ActionJoueur.cs
@@ -0,0 +1,20 @@
+﻿using SpaceAlert.Model.Helpers.Enums;
+
+namespace SpaceAlert.Model.Jeu
+{
+    /// <summary>
+    /// Représente une action en partie d'un joueur
+    /// </summary>
+    public class ActionJoueur
+    {
+        /// <summary>
+        /// Le type de l'action
+        /// </summary>
+        public TypeAction TypeAction { get; set; }
+
+        /// <summary>
+        /// La valeur de l'action
+        /// </summary>
+        public int Value { get; set; }
+    }
+}
---
+++ SpaceAlert.Model/Jeu/InGameJoueur.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using SpaceAlert.Model.Helpers.Enums;
+
+namespace SpaceAlert.Model.Jeu
+{
+    public class InGameJoueur
+    {
+        public Joueur Joueur { get; set; }
+
+        public List<ActionJoueur> Actions { get; set; }
+
+        public EtatRobots Robots { get; set; }
+    }
+}
